abc96e8314bb48febda1ce52e7e7ed3993f49599
wopp2 <lyricremenok@gmail.com>
2026-03-05 13:58:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const cards = document.querySelectorAll('.flip-card-inner');
-
-  cards.forEach(card => {
-    card.addEventListener('click', () => {
-      card.classList.toggle('flipped');
-    });
-  });
-});
